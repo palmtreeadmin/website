0e49305a81c163522c8646dd65f08836b378a229
Rajesh Koilpillai <rajesh.koilpillai@gmail.com>
2014-05-12 09:37:14 +0530

    added arrows

---
+++ js/init.js
@@ -0,0 +1,18 @@
+$(function(){	
+	$('.slider').mobilyslider({
+		content: '.sliderContent',
+		children: 'div',
+		transition: 'horizontal',
+		animationSpeed: 800,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnHover: true,			
+		bullets: true,
+		arrows: true,
+		arrowsHide: true,
+		prev: 'prev',
+		next: 'next',
+		animationStart: function(){},
+		animationComplete: function(){}
+	});	
+});
